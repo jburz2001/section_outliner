36c3ba7354e4bc643ac6e2cfb324198bf5f36ec8
Justin Burzachiello <justinburzachiello@gmail.com>
2021-12-01 11:33:17 -0800

    segments image by k-means

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-#https://www.youtube.com/watch?v=-2uyzAqefyE&list=PLzMcBGfZo4-lB8MZfHPLTEHO9zJDDLpYj&index=2
+#https://medium.com/nerd-for-tech/dominant-color-extraction-for-img-segmentation-2f30e9590889
+#https://www.thepythoncode.com/article/kmeans-for-img-segmentation-opencv-python
 
-import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+def imgFromPath(path):
+    img = cv2.imread(path,1)
+    orig_shape = img.shape
+    print(orig_shape)
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        x,y,w,h = 200,200,300,300
-        self.setGeometry(x,y,w,h)
-        winTitle = "Section Outliner"
-        self.setWindowTitle(winTitle)
-        self.initUi()
-        
-    def initUi(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("I am label")
-        self.label.move(50,50)
-        
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("Click me")
-        self.b1.clicked.connect(self.clicked)
-
-    def clicked(self):
-        self.label.setText("you pressed button")
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    return img
     
-def window():
-    app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
-    sys.exit(app.exec_())
+def segmentImg(img,k):
+    # convert image to linear array
+    pixel_values = np.float32(img.reshape((-1,3)))
+
+    # define stopping criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # convert back to 8 bit values
+    centers = np.uint8(centers)
+
+    # flatten the labels array
+    labels = labels.flatten()
+
+    # convert all pixels to the color of the centroids
+    seg_img = centers[labels.flatten()]
+
+    # reshape back to the original img dimension
+    seg_img = seg_img.reshape(img.shape)
     
-window()
+    return seg_img
+
+
+path = "afm.png"
+img = imgFromPath(path)
+plt.imshow(img)
+plt.show()
+
+ks = [2]
+for k in ks:
+    seg_img = segmentImg(img,k)
+    plt.imshow(seg_img)
+    plt.show()
