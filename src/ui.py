3e35eb97abee6d8b9422607870fbc2df9e62eb68
Justin Burzachiello <justinburzachiello@gmail.com>
2021-12-01 11:32:59 -0800

    moved afm.png for convienience

--- src/ui.py
+++ src/ui.py
@@ -3,9 +3,21 @@ import cv2 as cv
 class Ui():
     def __init__(self, path):
         self.path_ = path
+        #self.imgs_ = []
+        # self.base_img_
     
-    def print(self):
+    def printPath(self):
         print(self.path_)
-
-ui = Ui("hello")
-ui.print()
+        
+    def showFileFromPath(self,type):
+        #type
+        #   1=color
+        #   0=grayscale
+        #   -1=unchanged
+        img = cv.imread(self.path_, type)
+        cv.imshow("Image",img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+    
+ui = Ui("assets\afm.png")
+ui.showFileFromPath(1)
